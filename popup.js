d8569408b0258fb2bd8b9d314bca70ffa2fd2f8f
vitalij99 <vitalisklymko@gmail.com>
2023-12-16 15:03:45 +0200

    add popup

--- popup.js
+++ popup.js
@@ -1,16 +1,1 @@
-const runScriptButton = document.getElementById("start");
-
-runScriptButton.onclick = addScript();
-function addScript() {
-  // Додати скрипт на сторінку
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: executeCode,
-    });
-  });
-}
-function executeCode() {
-  // Ваш код, який буде виконано на сторінці
-  console.log("Script executed on the page!");
-}
+console.log("popup js");
