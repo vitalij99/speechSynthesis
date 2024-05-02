a6de16176b5076f65c23f9281613ab0d005fb219
vitalij99 <vitalisklymko@gmail.com>
2024-05-02 12:54:38 +0300

    add control btn

--- popup.js
+++ popup.js
@@ -1,15 +1,25 @@
-document.getElementById("startReader").addEventListener("click", function () {
+const btnStartReader = document.getElementById("startReader");
+
+btnStartReader.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "startReadeFun" });
   });
+  btnStartReader.disabled = true;
 });
 
 getStorage().then(({ options }) => {
   const btnBook = document.querySelector(".book-popup");
-  const { book, bookURL } = options;
+  const { book, bookURL, reade } = options;
 
   btnBook.innerText = book ? book : "books";
   btnBook.href = bookURL;
+
+  if (!reade) return;
+
+  const dateSave = new Date(options.reade);
+  const dateNow = new Date();
+
+  btnStartReader.disabled = dateNow > dateSave ? false : true;
 });
 
 async function getStorage() {
