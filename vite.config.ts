46327a14b9c4c417f9038b29a0b1548076416c25
vitalij99 <vitalisklymko@gmail.com>
2025-12-14 18:54:44 +0200

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: "build",
     emptyOutDir: true,
     rollupOptions: {
       input: {
