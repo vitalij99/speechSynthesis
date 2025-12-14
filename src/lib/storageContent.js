947da237dc619b72bbe30ccc1e2ca1129afb7e26
vitalij99 <vitalisklymko@gmail.com>
2025-12-14 16:12:15 +0200

    add params content

---
+++ src/lib/storageContent.js
@@ -0,0 +1,24 @@
+export const navigator = {
+  nextPageSave: null,
+  thisPageSave: null,
+  bookURL: null,
+  book: null,
+};
+
+export const mouse = { x: 0, y: 0 };
+
+export const options = {
+  contentDivElem: "#content  ",
+  nextPageBtn: ".nextchap",
+  timer: 20,
+  lastParagraf: 0,
+  timerCheckbox: true,
+  timeout: 2000,
+  utterThis: {
+    language: null,
+    pitch: 2,
+    rate: 2,
+    volume: 1,
+  },
+};
+// ----------------------------------------
